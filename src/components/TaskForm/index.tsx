5892e232e8f1487d941d46a349548d4d17e53c0b
Pietro Bergamaschi <pietrovb25@gmail.com>
2023-03-31 21:11:59 +0044

    CSS do formulário

--- src/components/TaskForm/index.tsx
+++ src/components/TaskForm/index.tsx
@@ -9,21 +9,19 @@ interface Props {
 const TaskForm = ({ btnText }: Props) => {
   return (
     <Container>
-      <form>
-        <div>
-          <label htmlFor="title">Título:</label>
-          <input type="text" name="title" placeholder="Título da tarefa" />
-        </div>
-        <div>
-          <label htmlFor="difficulty">Dificuldade:</label>
-          <input
-            type="number"
-            name="difficulty"
-            placeholder="Dificuldade da tarefa (1 a 5)"
-          />
-        </div>
-        <input type="submit" value={btnText} />
-      </form>
+      <div className="input-container">
+        <label htmlFor="title">Título:</label>
+        <input type="text" name="title" placeholder="Título da tarefa" />
+      </div>
+      <div className="input-container">
+        <label htmlFor="difficulty">Dificuldade:</label>
+        <input
+          type="number"
+          name="difficulty"
+          placeholder="Dificuldade da tarefa"
+        />
+      </div>
+      <input type="submit" value={btnText} />
     </Container>
   );
 };
--- src/components/TaskForm/styles.ts
+++ src/components/TaskForm/styles.ts
@@ -1,3 +1,39 @@
 import styled from "styled-components";
 
-export const Container = styled.div``;
+export const Container = styled.form`
+  display: flex;
+  flex-direction: column;
+  max-width: 400px;
+  margin: 0 auto;
+
+  input {
+    padding: 8px 15px;
+    margin-bottom: 1.5em;
+    border-radius: 0;
+    border: 1px solid #282c34;
+  }
+
+  input[type="submit"] {
+    background-color: #61bafb;
+    border: 1px solid #61bafb;
+    color: #000;
+    transition: 0.5s;
+    cursor: pointer;
+  }
+
+  input[type="submit"]:hover {
+    background-color: #fff;
+    border: 1px solid #282c34;
+  }
+
+  .input-container {
+    display: flex;
+    flex-direction: column;
+    text-align: left;
+  }
+
+  .input-container label {
+    font-weight: bold;
+    margin-bottom: 0.4em;
+  }
+`;
