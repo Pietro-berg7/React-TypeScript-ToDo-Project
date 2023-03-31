f69f74105470758117598de402edfaf83dc16227
Pietro Bergamaschi <pietrovb25@gmail.com>
2023-03-31 20:10:52 +0044

    Criando Header e Footer

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+// Components
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <h1>Conteudo...</h1>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
